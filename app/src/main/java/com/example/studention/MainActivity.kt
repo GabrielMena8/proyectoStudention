f578098704801145f95e108e8f0d48cb5dd29305
Andres Leonardo Balbuena Valera <77251658+AndresBLV@users.noreply.github.com>
2024-10-16 20:06:16 -0400

    Revert "TextFields/Login kinda"
    This reverts commit d90f21bdb289cb94119643f2ea66f7ef48e11f10.

--- app/src/main/java/com/example/studention/MainActivity.kt
+++ app/src/main/java/com/example/studention/MainActivity.kt
@@ -1,37 +1,17 @@
 package com.example.studention
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.provider.Contacts.Intents.UI
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.studention.ui.theme.StudentionTheme
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TextField
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,89 +19,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StudentionTheme {
-               App()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
-
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 
 @Composable
-fun App() {
-    var email = remember { mutableStateOf(TextFieldValue(" ")) }
-    var password = remember { mutableStateOf(TextFieldValue(" ")) }
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 32.dp)
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
     )
-    {
-        Image(
-            painter = painterResource(id = R.drawable.c),
-            contentDescription = "Logo",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-
-            ) {
-
-
-          /*  Text(
-                text = "Hello, ${Texto.value.text}!",
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(top = 32.dp),
-                color = Color.White,
-
-
-
-            )*/
-            TextField(
-                value = email.value,
-                onValueChange = { email.value = it },
-                label = { Text("Enter your name") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 64.dp)
-
-            )
-
-            TextField(
-                value = password.value,
-                onValueChange = { password.value = it },
-                label = { Text("Ingresa tu Contraseña") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp)
-
-            )
-
-            Spacer(modifier = Modifier.weight(3f))
-
-
-
-            Button(
-                onClick = {
-                   /* email.value = TextFieldValue("Hello, ${Texto.value.text}!")*/
-                },
-                modifier = Modifier.align(Alignment.End)
-
-            ) {
-                Text("Say Hello")
-            }
-
-        }
-    }
 }
 
-
-
-
-
-
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StudentionTheme {
+        Greeting("Android")
+    }
+}
