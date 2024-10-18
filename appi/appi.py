37fd52b33ae3f7612664dd9f300381430f1a5c3e
Andres Leonardo Balbuena Valera <77251658+AndresBLV@users.noreply.github.com>
2024-10-18 17:19:11 -0400

    Api con mas forma de data

--- appi/appi.py
+++ appi/appi.py
@@ -16,14 +16,18 @@ class Vote(BaseModel):
 def read_root():
     return {"Welcome": "Welcome to Studention API"}
 
-@app.get("/vote")
+@app.get("/vote/")
 def get_vote():
     return Vote
 
+@app.post("/vote/")
+def create_vote(vote: Vote):
+    return vote
+
 @app.get("/vote/{vote_id}")
 def read_vote(vote_id: int):
-    return {"Vote": vote_id, "Code": Vote.code}
+    return {"Vote": vote_id, "Code": Vote.code, "Vote": Vote.vote}
 
 @app.put("/vote/{vote_id}")
-def update_item(vote:Vote):
-    return {"Message": f"Voto {Vote.code} insertado"}
+def update_item(vote_id: int, vote: Vote):
+    return {"Vote_id": vote_id, "Code": Vote.code}
