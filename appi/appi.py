d047458b9b29a01dcbf01e2e2bba237e743f8a15
Andres Leonardo Balbuena Valera <77251658+AndresBLV@users.noreply.github.com>
2024-10-21 20:24:01 -0400

    Reajuste de la API

--- appi/appi.py
+++ appi/appi.py
@@ -7,8 +7,8 @@ app = FastAPI()
 vote = []
 class Vote(BaseModel):
     code: str = None
-    boton1: str
-    boton2: str
+    boton1: bool
+    boton2: bool
 
 def random_code():
   random_list = []
@@ -33,18 +33,32 @@ def read_root():
     return {"Welcome": "Welcome to Studention API"}
 
 @app.get("/vote/")
-def get_vote():
-    return Vote
+def get_code():
+    return Vote, Vote.code
 
 @app.post("/vote/")
 def create_vote(vote: Vote):
     vote.code = random_code()
     return Vote
 
-@app.get("/vote/{vote_id}")
-def read_vote(vote_id: int):
-    return {"Vote": vote_id, "Code": Vote.code, "Boton1": Vote.boton1, "Boton2": Vote.boton1}
+@app.post("/vote/1")
+def create_vote(codeESP, vote: Vote):
+    vote.code = codeESP
+    vote.boton1 = True
+    vote.boton2 = False
+    return Vote
 
-@app.put("/vote/{vote_id}")
-def update_item(vote_id: int, vote: Vote):
+@app.post("/vote/2")
+def create_vote(codeESP, vote: Vote):
+    vote.code = codeESP
+    vote.boton1 = True
+    vote.boton2 = False
+    return Vote
+
+@app.get("/vote/{id}")
+def read_vote(id: int):
+    return {"Vote": id, "Code": Vote.code, "Boton1": Vote.boton1, "Boton2": Vote.boton1}
+
+@app.put("/vote/{id}")
+def update_item(id: int, vote: Vote):
     return {"Vote_id": vote_id, "Code": Vote.code, "Boton1": Vote.boton1, "Boton2": Vote.boton1}
