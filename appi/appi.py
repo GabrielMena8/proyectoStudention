ba20121b1fad19377176d466dbfacd9aacdd5947
David30821945 <122551334+David30821945@users.noreply.github.com>
2024-11-04 18:47:26 -0400

    sin firebase

--- appi/appi.py
+++ appi/appi.py
@@ -2,13 +2,9 @@ from random import randint
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
-import firebase_admin 
-from firebase_admin import credentials, db
 
-cred = credentials.Certificate("path/to/your-firebase-adminsdk.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://your-database-name.firebaseio.com'
-})
+
+
 
 
 app = FastAPI()
@@ -32,9 +28,7 @@ def read_root():
 def get_votes():
     return votes
 
-def update_firebase(vote): 
-    ref = db.reference(f'votes/{vote["id"]}') 
-    ref.set(vote)
+
 
 @app.post("/votes")
 def save_vote(vote: Vote):
@@ -57,14 +51,8 @@ def update_vote1(vote_id: str):
             return vote
     return {"message": "Vote not found"}
 
-@app.put("/votes/{vote_id}/button1") 
-def update_vote1(vote_id: str): 
-    for vote in votes: 
-        if vote["id"] == vote_id: 
-            vote["boton1"] += 1 
-            update_firebase(vote) 
-            return vote 
-        return {"message": "Vote not found"}
+
+
 @app.put("/votes/{vote_id}")
 def update_vote2(vote_id: str):
     for vote in votes:
@@ -73,11 +61,3 @@ def update_vote2(vote_id: str):
             return vote
     return {"message": "Vote not found"}
 
-@app.put("/votes/{vote_id}/button2") 
-def update_vote2(vote_id: str): 
-    for vote in votes: 
-        if vote["id"] == vote_id: 
-            vote["boton2"] += 1 
-            update_firebase(vote) 
-            return vote 
-        return {"message": "Vote not found"}
