89e8d021944a47c256a3bfb9e44a88be10e0597c
Andres Leonardo Balbuena Valera <77251658+AndresBLV@users.noreply.github.com>
2024-11-04 22:28:53 -0400

    Probar API

--- appi/appi.py
+++ appi/appi.py
@@ -49,7 +49,7 @@ def get_vote(vote_id: int):
             return vote
     return {"message": "Vote not found"}
 
-@app.put("/votes/{vote_id}/1") 
+@app.put("/votes/{vote_id}/boton1") 
 def update_vote1(vote_id: str): 
     for vote in votes: 
         if vote["id"] == vote_id: 
@@ -58,7 +58,7 @@ def update_vote1(vote_id: str):
             return vote 
         return {"message": "Vote not found"}
 
-@app.put("/votes/{vote_id}/2") 
+@app.put("/votes/{vote_id}/boton2") 
 def update_vote2(vote_id: str): 
     for vote in votes: 
         if vote["id"] == vote_id: 
